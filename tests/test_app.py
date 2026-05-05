9e4410494e130b6c755773fb6f03eb4395ea1c96
Ashok Reddy <lakkireddy223@gmail.com>
2026-05-05 10:07:43 +0530

    fifth commit - prod

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json["message"] == "Production Ready CI/CD Flask App - lakkireddy"
+    assert response.json["message"] == "Production Ready CI/CD Flask AppL"
 
 
 def test_health():
