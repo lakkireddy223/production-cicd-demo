b5329e554b0e42f88bfb4f5b23a1dbebca7ec5a6
Ashok Reddy <lakkireddy223@gmail.com>
2026-05-07 08:18:09 +0530

    env variables test

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,44 @@
-
+import os
 from flask import Flask, jsonify
 
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["APP_NAME"] = os.getenv(
+        "APP_NAME",
+        "Production Flask App"
+    )
+
     @app.route("/")
     def home():
         return jsonify({
-            "message": "Production Ready CI/CD Flask App is doing by Yuvansh and Janvika"
-        }), 200
+            "message": app.config["APP_NAME"]
+        })
 
     @app.route("/health")
     def health():
         return jsonify({
             "status": "healthy"
-        }), 200
+        })
 
     return app
+# from flask import Flask, jsonify
+
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     @app.route("/")
+#     def home():
+#         return jsonify({
+#             "message": "Production Ready CI/CD Flask App is doing by Yuvansh and Janvika"
+#         }), 200
+
+#     @app.route("/health")
+#     def health():
+#         return jsonify({
+#             "status": "healthy"
+#         }), 200
+
+#     return app
