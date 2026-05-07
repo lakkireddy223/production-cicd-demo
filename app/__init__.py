d07e68a2abf7ba34d93cfc1f452ddfd36741eb3b
Ashok Reddy <lakkireddy223@gmail.com>
2026-05-07 09:50:11 +0530

    RDS data added

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 import os
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
@@ -9,6 +13,15 @@ def create_app():
         "APP_NAME",
         "Production Flask App"
     )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///local.db"
+    )
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
 
     @app.route("/")
     def home():
@@ -22,7 +35,50 @@ def create_app():
             "status": "healthy"
         })
 
+    @app.route("/db-check")
+    def db_check():
+        try:
+            db.session.execute("SELECT 1")
+
+            return jsonify({
+                "database": "connected"
+            })
+
+        except Exception as e:
+            return jsonify({
+                "database": "failed",
+                "error": str(e)
+            }), 500
+
+    with app.app_context():
+        db.create_all()
+
     return app
+# import os
+# from flask import Flask, jsonify
+
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     app.config["APP_NAME"] = os.getenv(
+#         "APP_NAME",
+#         "Production Flask App"
+#     )
+
+#     @app.route("/")
+#     def home():
+#         return jsonify({
+#             "message": app.config["APP_NAME"]
+#         })
+
+#     @app.route("/health")
+#     def health():
+#         return jsonify({
+#             "status": "healthy"
+#         })
+
+#     return app
 # from flask import Flask, jsonify
 
 
